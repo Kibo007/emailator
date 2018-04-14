90904ac7b18cc21a08e952cb9cfcb5d58a8c6b64
Bojan Dragojevic <bdragojevic@gmail.com>
2018-04-14 21:07:08 +0200

    setup google oauth

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const authRoutes = require('./router/auth');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there we changes' });
-});
+// import model users
+require('./models/Users');
+// Import passport
+require('./services/passport');
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookiesKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Bind routes
+app.get('auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ router/auth.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const router = require('express').Router;
+
+// google strategy
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['email', 'profile'],
+  })
+);
+router.get('/google/callback', passport.authenticate('google'));
+
+// logout
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
