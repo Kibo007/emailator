2682cb425b2c2aa5baeb5fa6a8995dbdcd54c7d2
Bojan Dragojevic <bdragojevic@gmail.com>
2018-04-14 22:26:48 +0200

    remove harcoded keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  googleClientID:
-    '349077292676-2hnabfmi120sn3dtecp0q80l9vdd51n2.apps.googleusercontent.com',
-  googleClientSecret: '_enCwb6zc5XPS01L1A6-N60s',
-  mongoURI: 'mongodb://kibo:kibo@ds259258.mlab.com:59258/emailator-dev',
-  cookiesKey: 'what-the-fuck-you-want',
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
+}
